a16d37d0a4c146038102e6eda9c30c6272b43462
Valdir Júnior <31621712+Valdjr@users.noreply.github.com>
2019-01-12 23:56:11 -0200

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,19 @@
+$(document).ready(() => {
+    $('.deleteUser').on('click', deleteUser);
+});
 
+function deleteUser() {
+    var confirmation = confirm('Are you sure?');
+
+    if(confirmation) {
+        $.ajax({
+            type: 'DELETE',
+            url: '/users/delete/'+$(this).data('id')
+        }).done((res) => {
+            window.location.replace('/');
+        });
+        window.location.replace('/');
+    } else {
+        return false;
+    }
+}
